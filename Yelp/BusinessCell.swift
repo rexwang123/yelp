551b2e06597af775b042c8f926cdcada77ecebdc
Jiayi Wang <jiayiwang@jiayis-mbp.attlocal.net>
2018-02-19 21:23:12 -0800

    new

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,43 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Jiayi Wang on 2/19/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell{
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    var business: Business!{
+        didSet{
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
